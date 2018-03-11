fefaf217a99f84c148d61b9f3c11e17f27f16749
canlinz2 <https://gitlab.engr.illinois.edu>
2018-03-11 14:16:14 -0500

    MP3.1 PIC (done?)
    0.0.0.1j
    
    
    Signed-off-by: canlinz2 <https://gitlab.engr.illinois.edu>

--- student-distrib/i8259.c
+++ student-distrib/i8259.c
@@ -11,96 +11,66 @@ uint8_t slave_mask;  /* IRQs 8-15 */
 
 /* Initialize the 8259 PIC */
 void i8259_init(void) {
-  /* mask all of 8259A - 1 */
-  outb(0xFF, MASTER_8259_PORT + 1);
-  /* mask all of 8259A - 2 */
-  outb(0xFF, SLAVE_8259_PORT + 1);
+  outb(0xFF, MASTER_8259_DATA);
+  outb(0xFF, SLAVE_8259_DATA);
 
-  /* ICW1: select 8259A - 1 init */
   outb(ICW1, MASTER_8259_PORT);
-  /* ICW2: 8259A-1 IR0-7 mapped to 0x20 - 0x27*/
-  outb(ICW2_MASTER, MASTER_8259_PORT + 1);
-  /* 8259A - 1 has a slave on IR2 */
-  outb(ICW3_MASTER, MASTER_8259_PORT + 1);
+  outb(ICW2_MASTER, MASTER_8259_DATA);
+  outb(ICW3_MASTER, MASTER_8259_DATA);
+  outb(ICW4, MASTER_8259_DATA);
 
-  /* ICW1: select 8259A - 2 init */
   outb(ICW1, SLAVE_8259_PORT);
-  /* ICW2: 8259A-2 IR0-7 mapped to 0x28 - 0x2f */
-  outb(ICW2_SLAVE, SLAVE_8259_PORT + 1);
-  /* 8259A - 2 is slave on master's IR2 */
-  outb(ICW3_SLAVE, SLAVE_8259_PORT + 1);
+  outb(ICW2_SLAVE, SLAVE_8259_DATA);
+  outb(ICW3_SLAVE, SLAVE_8259_DATA);
+  outb(ICW4, SLAVE_8259_DATA);
 
-  /* Enable All on slave and master (Can be changed to other value after testing)*/
-  master_mask = 0x00;
-  slave_mask = 0x00;
+  master_mask = 0xFF;
+  slave_mask = 0xFF;
 
-  /* Restore master IRQ mask */
-  outb(master_mask, MASTER_8259_PORT + 1);
-  /* Restore slave IRQ mask */
-  outb(slave_mask, SLAVE_8259_PORT + 1);
+  outb(0xFF, MASTER_8259_DATA);
+  outb(0xFF, SLAVE_8259_DATA);
+
 }
 
 /* Enable (unmask) the specified IRQ */
 void enable_irq(uint32_t irq_num) {
-  if (irq_num < 0x28 && irq_num >= 0x20)
+  if (irq_num >= 0x00 && irq_num <= 0x07)
   {
-    /* Calculate new mask using bitwise and */
-    irq_num = irq_num - 0x20;
     master_mask = master_mask & (~(0x01 << irq_num));
-    outb(master_mask, MASTER_8259_PORT + 1);
+    outb(master_mask, MASTER_8259_DATA);
   }
-  if (irq_num >= 0x28 && irq_num <= 0x2F)
+  else if (irq_num >= 0x08 && irq_num <= 0x0F)
   {
-    /* Calculate new mask using bitwise and */
-    irq_num = irq_num - 0x28;
+    irq_num = irq_num - 0x08;
     slave_mask = slave_mask & (~(0x01 << irq_num));
-    outb(slave_mask, SLAVE_8259_PORT + 1);
+    outb(slave_mask, SLAVE_8259_DATA);
   }
-  return;
 }
 
 /* Disable (mask) the specified IRQ */
 void disable_irq(uint32_t irq_num) {
-  /* If irq_num is between 0x20 and 0x27, it is using master PIC. */
-  if (irq_num < 0x28 && irq_num >= 0x20)
+  if (irq_num >= 0x00 && irq_num <= 0x07)
   {
-    /* Calculate new mask using bitwise or. */
-    irq_num = irq_num - 0x20;
     master_mask = master_mask | (0x01 << irq_num);
-    outb(master_mask, MASTER_8259_PORT + 1);
+    outb(master_mask, MASTER_8259_DATA);
   }
-  /* If irq_num is between 0x28 and 0x2F, it is using slave PIC. */
-  if (irq_num >= 0x28 && irq_num <= 0x2F)
+  else if (irq_num >= 0x08 && irq_num <= 0x0F)
   {
-    /* Calculate new mask using bitwise or. */
-    irq_num = irq_num - 0x28;
+    irq_num = irq_num - 0x08;
     slave_mask = slave_mask | (0x01 << irq_num);
-    outb(slave_mask, SLAVE_8259_PORT + 1);
+    outb(slave_mask, SLAVE_8259_DATA);
   }
-  return;
 }
 
 /* Send end-of-interrupt signal for the specified IRQ */
 void send_eoi(uint32_t irq_num) {
-  /* If irq_num is between 0x20 and 0x27, it is using master PIC. */
-  if (irq_num < 0x28 && irq_num >= 0x20)
+  if (irq_num >= 0x00 && irq_num <= 0x07)
   {
-    /* Send EOI command (OCW2) to PIC */
-    /* Calculate IR level (L2 to L0) in OCW2 (EOI command) for master */
-    irq_num = irq_num - 0x20;
-    outb(EOI | irq_num, MASTER_8259_PORT);
+    outb(EOI + irq_num, MASTER_8259_PORT);
   }
-  /* If irq_num is between 0x28 and 0x2F, it is using slave PIC. */
-  if (irq_num >= 0x28 && irq_num <= 0x2F)
+  else if (irq_num >= 0x08 && irq_num <= 0x0F)
   {
-    /* Send EOI command (OCW2) to PIC */
-    /* Calculate IR level (L2 to L0) in OCW2 (EOI command) for slave*/
-    irq_num = irq_num - 0x28;
-    /* Send first EOI to master, IR level = ICW3_SLAVE */
-    outb(EOI | ICW3_SLAVE, MASTER_8259_PORT);
-    /* Send second EOI to slave, IR level calculated above. */
-    outb(EOI | irq_num, SLAVE_8259_PORT);
-
+    outb(EOI + irq_num - 0x08, SLAVE_8259_PORT);
+    outb(EOI + ICW3_SLAVE, MASTER_8259_PORT);
   }
-  return;
 }
--- student-distrib/i8259.h
+++ student-distrib/i8259.h
@@ -11,6 +11,8 @@
 /* Ports that each PIC sits on */
 #define MASTER_8259_PORT    0x20
 #define SLAVE_8259_PORT     0xA0
+#define MASTER_8259_DATA    0x21
+#define SLAVE_8259_DATA     0xA1
 
 /* Initialization control words to init each PIC.
  * See the Intel manuals for details on the meaning
