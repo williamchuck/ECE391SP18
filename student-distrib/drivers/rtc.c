211f3f6ef33687e1dbc4d2399239b145b8b3337a
hl13 <hl13@illinois.edu>
2018-03-12 03:35:42 -0500

    rtc done

--- student-distrib/drivers/rtc.c
+++ student-distrib/drivers/rtc.c
@@ -11,18 +11,22 @@
 #include "../idt/interrupt.h"
 extern void test_interrupts(void);
 void rtc_isr(){
-    printf("rtc int");
+    test_interrupts();
+    outb(0x0C, 0x70);   //select register C
+    inb(0x71);          //read register to clear interrupt
 }
 int rtc_enable_interrupt(){
     //referenced from https://wiki.osdev.org/RTC
     unsigned long flags;
     cli_and_save(flags);
 
-    outb(0x70, 0x8B);
-    char regB=inb(0x71);
-    outb(0x70, 0x8B);
-    outb(0x71, regB | 0x40);
-
+    outb(0x8B, 0x70);   //select register B, disable NMI
+    char regB=inb(0x71);//store current value
+    outb(0x8B, 0x70);   //select register B, disable NMI
+    outb(regB | 0x40, 0x71); //set Periodic Interrupt Enable bit and write it back
+    outb(0x0B, 0x70);//just to enable NMI
+    inb(0x71); //just read
     restore_flags(flags);
     return request_irq(8, &rtc_isr);
 }
+
