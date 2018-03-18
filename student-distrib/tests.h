09195d8fe02222fe7d06c5c4752ade4764fb177c
Zhang <yichi2@illinois.edu>
2018-03-18 16:24:24 -0500

    Disable all tests and print multi-boot info

--- student-distrib/tests.h
+++ student-distrib/tests.h
@@ -2,15 +2,15 @@
 #define TESTS_H
 
 /* Run tests for paging and IDT table */
-#define RUN_TESTS 1
+#define RUN_TESTS 0
 /* Run tests for RTC */
 #define RTC_TEST 0
 /* Do not show loading message */
-#define NOT_SHOW_MESSAGE 1
+#define NOT_SHOW_MESSAGE 0
 /* Run invalid page test (Will lead to Page Fault) */
 #define INVALID_ADDR_TEST 0
 /* Enable division by 0 test (Will trigger div by 0 handler) */
-#define DIV_0_TEST 1
+#define DIV_0_TEST 0
 // test launcher
 void launch_tests();
 
