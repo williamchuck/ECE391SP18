9d96bfe6b1dbf87b88e2db492ad6eb056dc9de15
Zhang <yichi2@illinois.edu>
2018-04-07 14:45:32 -0500

    Added process related file

---
+++ student-distrib/process/process.h
@@ -0,0 +1,20 @@
+#ifndef PROCESS_H_
+#define PROCESS_H_
+
+typedef PCB_block{
+	file_desc_t file_desc[8];
+	PCB_block_t* parent_PCB;
+	uint8_t* k_stack;
+	uint8_t* u_stack;
+} PCB_block_t;
+
+#define current_PCB \
+	((PCB_block_t *)get_current_PCB())
+
+static inline uint32_t get_current_PCB() {
+	uint32_t i, mask;
+	mask = 0xFFFFE000;
+	return ((uint32_t)(&i)) & mask;
+}
+
+#endif
