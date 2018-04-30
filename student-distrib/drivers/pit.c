dd2840545a73908608c36c32d1c3d47108a507fd
linz0306 <joanne0848@yahoo.com.tw>
2018-04-30 13:28:14 -0500

    magic number comments

--- student-distrib/drivers/pit.c
+++ student-distrib/drivers/pit.c
@@ -12,9 +12,9 @@
 #define MAX_DIV 1
 #define MIN_DIV 65535
 
-/* 
+/*
  * pit_isr:
- * Description: Service function for pit interrupt 
+ * Description: Service function for pit interrupt
  * Input: None
  * Output: None
  * Effect: Schedule on every pit interrupt
@@ -34,7 +34,7 @@ void pit_isr(){
 void init_pit(){
 	/* Install handler */
 	request_irq(0, &pit_isr);
-	
+
 	/* Initialize freq */
 	change_freq(50);
 
@@ -53,7 +53,7 @@ void change_freq(uint32_t new_freq){
 	uint16_t div;
 
 	/* Calculate divisor based on new_freq */
-	if(new_freq <= MIN_PIT_FREQ) 
+	if(new_freq <= MIN_PIT_FREQ)
 		div = MIN_DIV;
 	else if(new_freq >= MAX_PIT_FREQ)
 		div = MAX_DIV;
@@ -62,6 +62,8 @@ void change_freq(uint32_t new_freq){
 
 	/* Send signals to pit */
 	outb(RATE_COMMAND, PIT_COMMAND_PORT);
+	/* Send the last 8 bits of div data */
 	outb(div & 0x00FF, PIT_DATA_PORT);
+	/* Send the first 8 bits of div data */
 	outb(div >> 8, PIT_DATA_PORT);
 }
